9bb2d249f0ab93ad9c5542845dd13ad38113aa9f
Martin <advocd@gmail.com>
2023-05-12 19:43:06 +0200

    test

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+function handleButtonClick(event) {
+  console.log("clicked");
+  var clicker = document.getElementById('clicker');
+  console.log("est", clicker);
+  clicker.classList.remove('hidden');
+}
 
-$(document).ready(function() {
-    $("#clicker").click(function() {
-      $(this).toggleClass("visible");
-    });
-  });
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("test");
+  var clicker = document.getElementById('clicker');
+  console.log(clicker);
+  clicker.addEventListener("click", handleButtonClick);
+});
